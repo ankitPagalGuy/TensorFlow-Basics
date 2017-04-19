6ba6df69f81921564cef431647310527f63c54f8
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2017-04-19 09:05:28 +0530

    linear regression done

--- linear_regression.py
+++ linear_regression.py
@@ -17,7 +17,7 @@ train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
 train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
                          2.827,3.465,1.65,2.904,2.42,2.94,1.3])
 
-n_shape = train_X.shape[0]
+n_samples = train_X.shape[0]
  
 # tf Graph Input
 X = tf.placeholder("float")
@@ -46,7 +46,27 @@ with tf.Session() as sess:
 
 	#Fit all training data
 	for epoch in range(training_epochs):
-		for (x, y) in zip()
+		for (x, y) in zip(train_X, train_Y):
+			sess.run(optimizer, feed_dict={X:x , Y:y})
+
+		#Display logs per epoch step
+		if (epoch + 1) % display_step == 0:
+			c = sess.run(cost , feed_dict={X: train_X, Y: train_Y})
+			print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
+                "W=", sess.run(W), "b=", sess.run(b))
+
+	print("Optimization finished!")
+	training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+	print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+
+	#Graphic Display
+	plt.plot(train_X, train_Y, 'ro', label='Orignal data')
+	plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label = "Fitted Line")
+	plt.legend()
+	plt.show()
+
+
 
 
 
